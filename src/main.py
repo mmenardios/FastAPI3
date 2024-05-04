8aa7a3f563d37895779dcd286da4dc211a8dfb63
mmenardios <michel.menard@univ-lr.fr>
2024-05-04 12:34:58 +0200

    sans fonction tensor et indentation

--- src/main.py
+++ src/main.py
@@ -4,14 +4,17 @@ from pydantic import BaseModel
 import json
 
 import os
+
 print(os.getcwd())
- 
- class Input(BaseModel):
+
+
+class Input(BaseModel):
     text: str
- 
-#from src.utils.sentence_prediction import process_sentence, from_str_to_list, tags
-#from src.utils.model_trained import model_prediction
-#from src.utils.embedding import use_embed
+
+
+# from src.utils.sentence_prediction import process_sentence, from_str_to_list, tags
+# from src.utils.model_trained import model_prediction
+# from src.utils.embedding import use_embed
 
 app = FastAPI()
 
@@ -22,25 +25,25 @@ async def main():
 
 
 @app.post("/predict")
-#async def prediction(sentence: dict):
+# async def prediction(sentence: dict):
 async def submit(input: Input):
     # Turns the json input into a string object
-    #string_sentence = json.dumps(sentence)
+    # string_sentence = json.dumps(sentence)
 
     # process_sentence needs a string objet, and use_embed needs a list object
-    #processed_sentence = process_sentence(string_sentence)
+    # processed_sentence = process_sentence(string_sentence)
 
     # Suppression du premier mot "sentence" de la chaine de caractère
-    #words = processed_sentence.split(maxsplit=1)
-    #result_string = words[1]
+    # words = processed_sentence.split(maxsplit=1)
+    # result_string = words[1]
 
-    #list_sentence = from_str_to_list(result_string)
+    # list_sentence = from_str_to_list(result_string)
 
-    #embedded_sentence = use_embed(list_sentence)
+    # embedded_sentence = use_embed(list_sentence)
 
-    #model_output = model_prediction(embedded_sentence)
+    # model_output = model_prediction(embedded_sentence)
 
-    #tags_result = tags(model_output)
+    # tags_result = tags(model_output)
 
-    #return tags_result
+    # return tags_result
     return "test"
